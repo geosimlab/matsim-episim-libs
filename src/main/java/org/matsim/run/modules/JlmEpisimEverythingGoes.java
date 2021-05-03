ad16ca15bdcff8ddf0f05bd6de2afd8408408f18
Administrator <Administrator@132.66.180.109>
2021-05-03 11:11:00 +0300

    run id 33 calib param 0.0000011

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -60,7 +60,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions.csv";
 
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 32;
+	final public static String RUN_ID = "/" + 33;
 	/**
 	 * Activity names of the default params from
 	 * {@link #addDefaultParams(EpisimConfigGroup)}.
@@ -128,7 +128,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		episimConfig.setInitialInfections(100);
 		episimConfig.setFacilitiesHandling(EpisimConfigGroup.FacilitiesHandling.snz);
 		episimConfig.setSampleSize(1);
-		episimConfig.setCalibrationParameter(0.0000012);
+		episimConfig.setCalibrationParameter(0.0000011);
 		episimConfig.setInitialInfectionDistrict("yes");
 		
 		Map<LocalDate, Integer> infectionsPerDay = new TreeMap<LocalDate, Integer>();
