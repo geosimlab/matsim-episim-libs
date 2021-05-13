06c098be3f20766f1f5a4227e9790dd97e1409a3
Administrator <Administrator@132.66.180.109>
2021-05-13 13:28:13 +0300

    run 60 - ultra orthodox with july closure 0.5

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -64,7 +64,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 	
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 59 + "/" + 1;
+	final public static String RUN_ID = "/" + 60 + "/" + 1;
 	/**
 	 * Activity names of the default params from
 	 * {@link #addDefaultParams(EpisimConfigGroup)}.
@@ -152,11 +152,16 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		LocalDate openingDate= LocalDate.of(2020, 5, 5);
 		double group_a_open_rate_opening_date = 1;
 		double group_b_open_rate_opening_date = 1;
+		LocalDate closingDate2 = LocalDate.of(2020, 7, 6);
+		double group_a_open_rate_closing_date2 = 0.5;
+		double group_b_open_rate_closing_date2 = 1;
 		episimConfig.setPolicy(FixedPolicy.class, FixedPolicy.config()
 				.restrict(closingDate , group_a_open_rate_closing_date , group_a_activities)
 				.restrict(closingDate , group_b_open_rate_closing_date , group_b_activities)
 				.restrict(openingDate , group_a_open_rate_opening_date , group_a_activities)
 				.restrict(openingDate , group_b_open_rate_opening_date , group_b_activities)
+				.restrict(closingDate2 , group_a_open_rate_closing_date2 , group_a_activities)
+				.restrict(closingDate2 , group_b_open_rate_closing_date2 , group_b_activities)
 				.build()
 		);
 		return config;
