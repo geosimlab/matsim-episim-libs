3a94e3696c7e6e05fd7925317e59bb9ddaeac1fc
Administrator <Administrator@132.66.180.109>
2021-05-30 17:26:31 +0300

    run 93 haredi same as 91

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -66,7 +66,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 	
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 92 + "/" + 1;
+	final public static String RUN_ID = "/" + 93 + "/" + 1;
 	final public static int iterations = 300;
 	/**
 	 * Activity names of the default params from
@@ -158,7 +158,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		bind(ContactModel.class).to(HouseholdSecularContactModel.class).in(Singleton.class);
+		bind(ContactModel.class).to(HouseholdUltraOrthodoxContactModel.class).in(Singleton.class);
 		bind(ProgressionModel.class).to(AgeDependentProgressionModel.class).in(Singleton.class);
 		bind(InitialInfectionHandler.class).to(RandomInitialInfections.class).in(Singleton.class);
 	}
