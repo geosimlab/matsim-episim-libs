9f82811b1e4684b4a82f12d230bb31ea48eec3be
Administrator <Administrator@132.66.180.109>
2021-06-06 13:21:08 +0300

    run 108 adjustments

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -71,7 +71,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 107 + "/" + 1;
+	final public static String RUN_ID = "/" + 108 + "/" + 1;
 	final public static int iterations = 400;
 	/**
 	 * Activity names of the default params from
@@ -238,7 +238,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		double group_b_open_rate_opening_date = 1;
 		//		out of school
 		LocalDate closingDate2 = LocalDate.of(2020, 6, 21);
-		double group_a_open_rate_closing_date2 = 0.7;
+		double group_a_open_rate_closing_date2 = 0.6;
 		double group_b_open_rate_closing_date2 = 1;
 		LocalDate closingDate3 = LocalDate.of(2020, 7, 6);
 		double group_a_open_rate_closing_date3 = 0.4;
