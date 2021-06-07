11546388d4c56f252233dad0b2d63c9c3e68eabe
Administrator <Administrator@132.66.180.109>
2021-06-07 16:00:33 +0300

    run 111 infection groups 1.1 ultra

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -73,9 +73,9 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 110 + "/" + 1;
+	final public static String RUN_ID = "/" + 111 + "/" + 1;
 	final public static int iterations = 400;
-	final public static double ultraOrthodoxInfectionRate = 10;
+	final public static double ultraOrthodoxInfectionRate = 1.1;
 	final public static double secularInfectionRate = 1;
 	/**
 	 * Activity names of the default params from
