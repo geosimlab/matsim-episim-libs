1b117f0384b5156796dc387c77b1f98177b6fa4f
Administrator <Administrator@132.66.180.109>
2021-06-10 12:49:34 +0300

    run 123 like 117 with random vaccinations

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -74,7 +74,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 122 + "/" + 1;
+	final public static String RUN_ID = "/" + 123 + "/" + 1;
 	final public static int iterations = 400;
 	final public static double ultraOrthodoxInfectionRate = 1;
 	final public static double secularInfectionRate = 1;
@@ -239,7 +239,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		//		out of school
 		LocalDate closingDate2 = LocalDate.of(2020, 6, 21);
 		double group_secular_a_open_rate_closing_date2 = 0.6;
-		double group_ultra_a_open_rate_closing_date2 = 0.9;
+		double group_ultra_a_open_rate_closing_date2 = 0.8;
 		double group_b_open_rate_closing_date2 = 1;
 		LocalDate closingDate3 = LocalDate.of(2020, 7, 6);
 		double group_secular_a_open_rate_closing_date3 = 0.4;
@@ -247,7 +247,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		double group_b_open_rate_closing_date3 = 1;
 		LocalDate closingDate4 = LocalDate.of(2020, 8, 23);
 		double group_secular_a_open_rate_closing_date4 = 0.6;
-		double group_ultra_a_open_rate_closing_date4 = 0.9;
+		double group_ultra_a_open_rate_closing_date4 = 0.8;
 		double group_b_open_rate_closing_date4 = 1;
 		LocalDate closingDate5 = LocalDate.of(2020, 9, 28);
 		double group_secular_a_open_rate_closing_date5 = 0.3;
@@ -259,7 +259,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		double group_b_open_rate_closing_date6 = 1;
 		LocalDate closingDate7 = LocalDate.of(2020, 11, 27);
 		double group_secular_a_open_rate_closing_date7 = 0.7;
-		double group_ultra_a_open_rate_closing_date7 = 0.9;
+		double group_ultra_a_open_rate_closing_date7 = 0.8;
 		double group_b_open_rate_closing_date7 = 1;
 		LocalDate closingDate8 = LocalDate.of(2020, 12, 27);
 		double group_secular_a_open_rate_closing_date8 = 0.6;
