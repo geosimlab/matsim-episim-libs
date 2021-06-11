300af677e44475df01fe6b8abaf044011200fca8
Administrator <Administrator@132.66.180.109>
2021-06-11 13:27:58 +0300

    run 129 fix 128 mistake

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -74,7 +74,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 128 + "/" + 1;
+	final public static String RUN_ID = "/" + 129 + "/" + 1;
 	final public static int iterations = 170;
 	final public static double ultraOrthodoxInfectionRate = 1;
 	final public static double secularInfectionRate = 1;
@@ -265,7 +265,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		double group_secular_a_open_rate_closing_date8 = 0.6;
 		double group_ultra_a_open_rate_closing_date8 = 0.6;
 		double group_b_open_rate_closing_date8 = 1;
-		LocalDate closingDate9 = LocalDate.of(2020, 1, 8);
+		LocalDate closingDate9 = LocalDate.of(2021, 1, 8);
 		double group_a_open_rate_closing_date9 = 0.3;
 		double group_b_open_rate_closing_date9 = 1;
 		episimConfig.setPolicy(FixedPolicy.class, FixedPolicy.config()
