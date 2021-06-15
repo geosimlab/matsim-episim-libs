96c7f0ebfdbfbccd22541e9394710b1ce14f3ba6
Administrator <Administrator@132.66.180.109>
2021-06-15 11:59:20 +0300

    run 152 start 8 march

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -74,7 +74,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 151 + "/" + 1;
+	final public static String RUN_ID = "/" + 151 + "/" + 4;
 	final public static int iterations = 90;
 	final public static double ultraOrthodoxInfectionRate = 1;
 	final public static double secularInfectionRate = 1;
@@ -193,7 +193,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		config.network().setInputFile("C:/GeoSimLab/episim_jlm/Input_data/matsim_files/11.output_network.xml.gz");
 		config.plans().setInputFile("C:/GeoSimLab/episim_jlm/Input_data/matsim_files/population1.0_district_subpop_ultra_secular_equal.xml.gz");
 		String url = "C:/GeoSimLab/episim_jlm/Input_data/matsim_files/test.xml";
-		LocalDate startDate = LocalDate.of(2020, 3, 12);
+		LocalDate startDate = LocalDate.of(2020, 3, 8);
 //		LocalDate date = startDate;
 		episimConfig.setInputEventsFile(url);
 		episimConfig.setStartDate(startDate);
@@ -228,13 +228,13 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		String[] group_ultra_b_activities = {"pt", "work_internal_Ultra-Orthodox", "other_internal_Ultra-Orthodox", "fjlm_internal_Ultra-Orthodox", "tjlm_internal_Ultra-Orthodox",};
 		//		first clsure
 		LocalDate closingDate = LocalDate.of(2020, 3, 15);
-		double group_secular_a_open_rate_closing_date = 0.5;
-		double group_ultra_a_open_rate_closing_date = 0.5;
+		double group_secular_a_open_rate_closing_date = 0.2;
+		double group_ultra_a_open_rate_closing_date = 0.2;
 		double group_b_open_rate_closing_date = 1;
 //		//		end of first closure
 		LocalDate openingDate= LocalDate.of(2020, 5, 5);
-		double group_secular_a_open_rate_opening_date = 0.6;
-		double group_ultra_a_open_rate_opening_date = 0.6;
+		double group_secular_a_open_rate_opening_date = 0.4;
+		double group_ultra_a_open_rate_opening_date = 0.4;
 		double group_b_open_rate_opening_date = 1;
 //		//		out of school
 //		LocalDate closingDate2 = LocalDate.of(2020, 6, 21);
