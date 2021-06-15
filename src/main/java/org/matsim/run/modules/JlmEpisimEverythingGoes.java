9a21df265807e183d42bfa9739b06a9a003e6481
Administrator <Administrator@132.66.180.109>
2021-06-15 16:53:48 +0300

    run 162

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -74,7 +74,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 161 + "/" + 1;
+	final public static String RUN_ID = "/" + 162 + "/" + 1;
 	final public static int iterations = 90;
 	final public static double ultraOrthodoxInfectionRate = 1;
 	final public static double secularInfectionRate = 1;
@@ -229,7 +229,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		//		first clsure
 		LocalDate closingDate = LocalDate.of(2020, 3, 15);
 		double group_secular_a_open_rate_closing_date = 0.7;
-		double group_ultra_a_open_rate_closing_date = 0.8;
+		double group_ultra_a_open_rate_closing_date = 0.9;
 		double group_b_open_rate_closing_date = 1;
 //		//		end of first closure
 		LocalDate closingDateHarder= LocalDate.of(2020, 3, 25);
