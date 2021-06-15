ad470cea5b1efbc652917299bed4c14105dc101c
Administrator <Administrator@132.66.180.109>
2021-06-15 12:39:12 +0300

    run 154

--- src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
+++ src/main/java/org/matsim/run/modules/JlmEpisimEverythingGoes.java
@@ -74,7 +74,7 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 	final public static String JLM_RESTRICTIONS_GROUPS = "C:/GeoSimLab/episim_jlm/Input_data/raw/restrictions_groups.csv";
 
 	final public static String OUTPUT_FOLDER = "C:/GeoSimLab/episim_jlm/output";
-	final public static String RUN_ID = "/" + 153 + "/" + 1;
+	final public static String RUN_ID = "/" + 154 + "/" + 1;
 	final public static int iterations = 90;
 	final public static double ultraOrthodoxInfectionRate = 1;
 	final public static double secularInfectionRate = 1;
@@ -228,8 +228,8 @@ public class JlmEpisimEverythingGoes extends AbstractModule {
 		String[] group_ultra_b_activities = {"pt", "work_internal_Ultra-Orthodox", "other_internal_Ultra-Orthodox", "fjlm_internal_Ultra-Orthodox", "tjlm_internal_Ultra-Orthodox",};
 		//		first clsure
 		LocalDate closingDate = LocalDate.of(2020, 3, 15);
-		double group_secular_a_open_rate_closing_date = 0.3;
-		double group_ultra_a_open_rate_closing_date = 0.3;
+		double group_secular_a_open_rate_closing_date = 0.35;
+		double group_ultra_a_open_rate_closing_date = 0.35;
 		double group_b_open_rate_closing_date = 1;
 //		//		end of first closure
 		LocalDate openingDate= LocalDate.of(2020, 5, 5);
